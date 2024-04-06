19e92c5f98fbaffc005002632cf81d869b0ffe5c
David Xue <xuegdxw@gmail.com>
2024-04-06 14:03:00 -0400

    Nit

--- server/routes/command.py
+++ server/routes/command.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from server.services.pii_redaction import redact_command
+from services.pii_redaction import redact_command
 
 cmd_router = APIRouter()
 
---
+++ server/services/gen_cmd_summary.py
@@ -0,0 +1,1 @@
+"Generate Summary of Command"
